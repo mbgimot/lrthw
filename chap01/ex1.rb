c45e1967417153b653a301f592747654d2ca2673
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2016-12-08 16:03:42 +0000

    first

--- chap01/ex1.rb
+++ chap01/ex1.rb
@@ -1,10 +1,11 @@
-#puts "Hello World!"
-#puts "Hello Again"
-#puts "I loke typing this."
-#puts "This is fun."
-#puts "Yay! Printing."
-#puts "I'd much rather you 'not'."
-#puts 'I "said" do not touch this.'
-#study drill 1
-puts "another line"
-#hash comments out
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts "Another line"
+#to print one line just comment out using pounds the other lines.
+# you could also set each puts line to a variable and then just call puts on one variable
+# pound sign comments out a single line of code
---
+++ ex3.rb
@@ -0,0 +1,28 @@
+#will puts the string
+puts "I will now count my chickens:"
+#30/6 = 5 + 25 = 30. puts Hens 30
+puts "Hens #{25.0 + 30.0 / 6.0}"
+#-25*3 = 75, 75%4 = 3, 100-3 = 97. puts "Roosters 97"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+#puts string
+puts "Now I will count the eggs:"
+#puts integer(fixnum)
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+#puts string
+puts "Is it true that 3 + 2 < 5 - 7?"
+#puts integer(fixnum)
+puts 3.0 + 2.0 < 5.0 - 7.0
+#puts string and integer 5
+puts "What is 3 + 2? #{3.0 + 2.0}"
+#puts string and integer -2
+puts "What is 5 - 7? #{5.0 - 7.0}"
+#puts string
+puts "Oh, that's why it's false."
+#puts string
+puts "How about some more."
+#puts string + true
+puts "Is it greater? #{5.0 > -2.0}"
+#puts string + true
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+#puts string + false
+puts "Is it less or equal? #{5.0 <= -2.0}"
