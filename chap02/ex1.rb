42c9f7620e062623175835b8ceaa119dfff92dc6
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2016-12-08 16:07:27 +0000

    second

--- chap02/ex1.rb
+++
@@ -1,7 +0,0 @@
-# A comment, this is so you can read your progress
-
-puts "I could have code lke this"
-
-# puts "this wont run"
-
-puts "this will run."
---
+++ ex4.rb
@@ -0,0 +1,5 @@
+cars = 100
+space_in_a_car = 4.0
+divers = 30
+passengers = 90
+cars_not_driven = cars -drivers
